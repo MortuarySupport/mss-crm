7524cfa0fbaed9f19fc55a7c762010b241f0d60a
Scott Duncombe <scottnewmaclaptop@MacBookPro.net>
2026-06-25 11:50:53 +1000

    Remove overflowX hidden from case wrapper

--- src/App.jsx
+++ src/App.jsx
@@ -2500,7 +2500,7 @@ function MortuaryFlow({user,cases,onUpdateCase,onBack}) {
 
   return (
     <>
-    <div className="w-full px-3 py-4" style={{maxWidth:"100%",boxSizing:"border-box",overflowX:"hidden"}}>
+    <div className="w-full px-3 py-4" style={{maxWidth:"100%",boxSizing:"border-box"}}>
       <BackBtn onClick={()=>setSelCase(null)} label={`Back to ${selFH.name}`}/>
       <div className="mb-4"><CaseViewCard c={{...c,prep}} isAdmin={isAdmin} onSave={updates=>upd(c.id,updates)}/></div>
       {!c.acceptedAt&&(
